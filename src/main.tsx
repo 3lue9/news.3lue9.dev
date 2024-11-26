608d5d49a9cd4a2217efd3496463c55e5cbfcc8c
3lue9 <massimiliano@3lue9.dev>
2024-11-26 18:52:13 +0100

    analytics and speed :)

--- src/main.tsx
+++ src/main.tsx
@@ -7,8 +7,8 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
       <SpeedInsights/>
       <Analytics/>
+    <App />
   </StrictMode>,
 )
