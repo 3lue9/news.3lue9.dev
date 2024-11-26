145d0c619c8e70522b52734d11ea95a1830cfa2a
3lue9 <massimiliano@3lue9.dev>
2024-11-26 18:37:33 +0100

    initial commit vite + react + typescript

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [] = useState(0)
+
+  return (
+    <>
+      <h1 className={"title"}>lorem ipsum dolor </h1>
+    </>
+  )
+}
+
+export default App
